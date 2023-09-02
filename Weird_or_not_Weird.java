cc9eeeb9a3d4e027563bc659b672afea373f5f1b
Manasa <143861749+manasasuryasubrahmanayeswari@users.noreply.github.com>
2023-09-02 11:34:35 +0530

    import java.util.Scanner; public class weird {     public static void main(String args[])     {         int n;         Scanner sc=new Scanner(System.in);         n=sc.nextInt();         if(n%2==1)         {             System.out.println("weird");         }         else         {             System.out.println("not weird");  } } }

---
+++ Weird_or_not_Weird.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class weird
+{
+    public static void main(String args[])
+    {
+        int n;
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+        if(n%2==1)
+        {
+            System.out.println("weird");
+        }
+        else
+        {
+            System.out.println("not weird");
+ }
+}
+}
