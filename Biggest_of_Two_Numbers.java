2e62a8c0ab6d4f0e66facae5192c0ed37a2f0d72
Manasa <143861749+manasasuryasubrahmanayeswari@users.noreply.github.com>
2023-09-02 11:32:29 +0530

    import java.util.Scanner; public class number {     public static void main(String args[])     {         int n1,n2;         Scanner sc=new Scanner(System.in);         n1=sc.nextInt();         n2=sc.nextInt();         if(n1>n2)         {             System.out.println(n1);         }         else         {             System.out.println(n2);  } } }

---
+++ Biggest_of_Two_Numbers.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class number
+{
+    public static void main(String args[])
+    {
+        int n1,n2;
+        Scanner sc=new Scanner(System.in);
+        n1=sc.nextInt();
+        n2=sc.nextInt();
+        if(n1>n2)
+        {
+            System.out.println(n1);
+        }
+        else
+        {
+            System.out.println(n2);
+ }
+}
+}
+
