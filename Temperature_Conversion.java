4e352936ac5403289572b1fad0c9617ffc38e8de
Manasa <143861749+manasasuryasubrahmanayeswari@users.noreply.github.com>
2023-10-05 09:49:39 +0530

    import java.util.*; public class temp {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         double C=sc.nextDouble();         double F=32+(C*9/5);         System.out.printf("%.2f",F);     } }

---
+++ Temperature_Conversion.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class temp
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        double C=sc.nextDouble();
+        double F=32+(C*9/5);
+        System.out.printf("%.2f",F);
+    }
+}
