fa7df06d35d91b6cea743c9e4c16dab96f46af05
Manasa <143861749+manasasuryasubrahmanayeswari@users.noreply.github.com>
2023-11-05 18:31:41 +0530

    import java.util.Scanner; public class triangle { public static void main (String args[]){     Scanner sc =new Scanner(System.in);     int a=sc.nextInt();     int b=sc.nextInt();     int c=sc.nextInt();     if(a==b&b==c)     {         System.out.println("Equilateral triangle");     }      else if(a==b||a==c||b==c)      {          System.out.println("Isosceles triangle");      }      else      {          System.out.println("Scalene triangle");      } } }

--- Triangle.java
+++ Triangle.java
@@ -1,23 +1,22 @@
-import java.util.*;
+import java.util.Scanner;
 public class triangle
 {
-    public static void main(String[] args)
+public static void main (String args[]){ 
+    Scanner sc =new Scanner(System.in);
+    int a=sc.nextInt();
+    int b=sc.nextInt();
+    int c=sc.nextInt();
+    if(a==b&b==c)
     {
-        Scanner sc=new Scanner(System.in);
-        int a=sc.nextInt();
-        int b=sc.nextInt();
-        int c=sc.nextInt();
-        if(a==b&b==c)
-        {
-            System.out.println("Equilateral triangle");
-        }
-        else if(a==b|a==c|b==c)
-        {
-            System.out.println("Isosceles triangle");
-        }
-        else
-        {
-            System.out.println("Scalene triangle");
-        }
+        System.out.println("Equilateral triangle");
     }
+     else if(a==b||a==c||b==c)
+     {
+         System.out.println("Isosceles triangle");
+     }
+     else
+     {
+         System.out.println("Scalene triangle");
+     }
+}
 }
